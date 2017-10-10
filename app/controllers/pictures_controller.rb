468e99dbb80d62c3a1065bbb6bfcb7882ceb308c
Omair <omairraza9@gmail.com>
2017-10-10 16:50:26 -0400

    creating edit capeability

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,5 +6,34 @@ class PicturesController < ApplicationController
     def show
       @picture = Picture.find(params[:id])
     end
-    
+
+    def new
+      @picture = Picture.new
+    end
+
+    def create
+    end
+
+    def create
+      render text: "Received POST request to '/pictures' with the data URL: #{params}"
+    end
+
+    def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+
+    if @picture.save
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
 end
